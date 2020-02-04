bf4f801425c8082820aa4ce7e29278b27b50dd63
Minki96 <46333497+Minki96@users.noreply.github.com>
2020-02-04 20:08:35 +0900

    Add files via upload

---
+++ my01/src/데이터/기본데이터.java
@@ -0,0 +1,10 @@
+package 데이터; 
+
+public class 기본데이터 {
+
+	public static void main(String[] args) {
+		System.out.println("HelloWorld...");
+		System.out.println("또 만났네요...");
+	}
+
+}
---
+++ my01/src/데이터/나.java
@@ -0,0 +1,25 @@
+package 데이터;
+
+public class 나 {
+
+	public static void main(String[] args) {
+		// 기본 데이터 연습
+		// 저장공간 => 변수
+		// 저장공간이름 => 변수명
+		
+		int 나이 = 24;
+		double 시력 = 0.2; //실수	
+		char 성별 = '남';
+		boolean 저녁 = true ; // 논리형
+		
+		// 실행(run) 컨트롤 + f11
+		// 자동완성
+		// syso + 컨트롤 + 스페이스바
+		System.out.println("내 나이는 "+ 나이 + "세");
+		System.out.println("내 시력은 "+ 시력);
+		System.out.println("내 성별은 "+ 성별 + "성");
+		System.out.println("오늘 저녁을 먹었나요? "+ 저녁);
+
+	}
+
+}
